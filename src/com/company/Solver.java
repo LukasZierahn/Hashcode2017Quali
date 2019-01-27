7e98ba256205118bb687ce07a878c121ce8d86fd
Lukas Zierahn <lukas@kappa-mm.de>
2019-01-27 15:09:15 +0000

    Outlining the project

---
+++ src/com/company/Solver.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.List;
+
+public class Solver {
+    List videos; //this should be a list of videos from the videos class
+
+    public void readWorldStateFromFile(String path) {
+        //this has to read the file into variables in this class and into a worldstate!
+    }
+}
---
+++ src/com/company/WorldState.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class WorldState {
+    //there have to be a copy of all caches and connections in here and a function to deep copy them into a new worldstate
+}
