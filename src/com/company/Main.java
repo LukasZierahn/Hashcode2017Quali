636d839a4f9cdf06b212c568d8a1b128ba48906c
Lukas Zierahn <lukas@kappa-mm.de>
2019-01-27 14:47:31 +0000

    Testcommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("test");
 
-	System.out.println("this is something new!");
+	System.out.println("this is bullcrap!");
     }
 }
