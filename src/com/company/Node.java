f15390628c0ad1dc43f60637034f22e9f97a0d99
Lukas Zierahn <lukas@kappa-mm.de>
2019-01-27 15:15:54 +0000

    Outlining the project #2

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,6 +1,12 @@
 package com.company;
 
+import java.util.List;
+
 public class Node {
     Node previousNode;
     int depth = -1;
+
+    int score = 0;
+
+    List<Node> outgoingNodes;
 }
--- src/com/company/Solver.java
+++ src/com/company/Solver.java
@@ -3,7 +3,15 @@ package com.company;
 import java.util.List;
 
 public class Solver {
-    List videos; //this should be a list of videos from the videos class
+    List<Video> videos; //this should be a list of videos from the videos class
+    Node baseNode;
+
+    int currentHighScore = 0;
+
+    Solver() {
+        baseNode.depth = 0;
+        baseNode.previousNode = null;
+    }
 
     public void readWorldStateFromFile(String path) {
         //this has to read the file into variables in this class and into a worldstate!
